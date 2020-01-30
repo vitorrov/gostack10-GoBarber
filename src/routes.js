802384abcac6e341470f9c527e7f2ff39b584df2
Vitor <vitormr45@gmail.com>
2020-01-30 01:47:08 -0300

    Minimal updates

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.get('/teste'); // Rota de testes
 
 routes.post('/users', UserController.store); // Criar usuário
+routes.post('/sessions', SessionController.store); // Login
 
 export default routes;
