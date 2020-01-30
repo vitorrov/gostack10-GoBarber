83e2b603754dd2490828e7a83977e87a54fce454
Vitor <vitormr45@gmail.com>
2020-01-30 18:30:42 -0300

    Minimal updates

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.get('/teste'); // Rota de testes
 
@@ -15,5 +20,7 @@ routes.post('/sessions', SessionController.store); // Login
 routes.use(authMiddleware); // Todas rotas após essa linha passarão pelo authMiddleware
 
 routes.put('/users', authMiddleware, UserController.update); // Alterar dados
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
