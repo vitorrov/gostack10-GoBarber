51cec45cd7c293989851455210a55654b9eba928
Vitor <vitormr45@gmail.com>
2020-02-03 17:29:50 -0300

    Nodemail configuration

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '692363a86dfd5a',
+    pass: '5b59264e4dacc6',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
